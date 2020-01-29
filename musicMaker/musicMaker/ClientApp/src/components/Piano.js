979641828840e3206ec65a287d8a025067190603
Ba Matrai <matrai.barnabas1@gmail.com>
2020-01-29 10:10:40 +0100

    add piano route

---
+++ musicMaker/musicMaker/ClientApp/src/components/Piano.js
@@ -0,0 +1,13 @@
+﻿import React, { Component } from 'react';
+
+export class Piano extends Component {
+    displayName = Piano.name
+
+    render() {
+        return (
+            <div>
+                <p>Hi</p>
+            </div>
+        );
+    }
+}
