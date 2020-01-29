2e00fd11ca115ed069af21f635f0984a3dbb0cf9
Ba Matrai <matrai.barnabas1@gmail.com>
2020-01-29 10:58:25 +0100

    add default click event to C key

--- musicMaker/musicMaker/ClientApp/src/components/Piano.js
+++ musicMaker/musicMaker/ClientApp/src/components/Piano.js
@@ -3,10 +3,17 @@
 export class Piano extends Component {
     displayName = Piano.name
 
+    playSound() {
+        alert("Die");
+    }
+
     render() {
         return (
             <div>
-                <img src={require('C:/Users/matra/OneDrive/Asztali gép/codecool/PET_projects/musicMaker/musicMaker/musicMaker/ClientApp/src/images/piano.png')} alt='piano' />
+                <img src={require('C:/Users/matra/OneDrive/Asztali gép/codecool/PET_projects/musicMaker/musicMaker/musicMaker/ClientApp/src/images/piano.png')} alt='piano' useMap='#keys' />
+                <map name="keys">
+                    <area shape="rect" coords="8,8,38,140" alt="C" onClick={this.playSound} />
+                </map>
             </div>
         );
     }
